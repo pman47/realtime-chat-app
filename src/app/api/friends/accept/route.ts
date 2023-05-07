02bab794a88975d2551f0143edaa80a104fd5906
Manish Prajapati <manishprajapatim416@gmail.com>
2023-05-07 16:01:56 +0530

    Refreshing window when user accepts friend request

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 
@@ -34,11 +36,15 @@ export async function POST(req: Request) {
       return new Response("No friend request", { status: 400 });
     }
 
+    // Notify added user
+    pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:friends`),
+      `new-friend`,
+      {}
+    );
+
     await db.sadd(`user:${session.user.id}:friends`, idToAdd);
     await db.sadd(`user:${idToAdd}:friends`, session.user.id);
-
-    // await db.srem(`user:${idToAdd}:outbound_friend_requests`, session.user.id);
-
     await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd);
 
     return new Response("ok");
